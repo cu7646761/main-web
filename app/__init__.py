7ceba7d28dfeab0fffc6b4c78bb7ef2716ebb3dd
phuongvuong98 <vuongnp@athena.studio>
2020-02-22 20:53:47 +0700

    comp generate data

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,26 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 from elasticsearch import Elasticsearch
 from config import config
 # from app.cache import cache
 from flask_bcrypt import Bcrypt
+from flask_session import Session
 
 db = MongoEngine()
 bcrypt = Bcrypt()
+sess = Session()
 
 
 def create_app(config_name):
     app = Flask(__name__)
-    CORS(app)
+    app.config['SECRET_KEY'] = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
     bcrypt.init_app(app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     db.init_app(app)
     # cache.init_app(app)
+    sess.init_app(app)
 
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
--- flasky.py
+++ flasky.py
@@ -1,14 +1,10 @@
-from flask_mongo_sessions import MongoDBSessionInterface
-
-from app import create_app, cache, db
+from app import create_app, cache
 import os
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
-with app.app_context():
-    mongo_sess = db.connection[app.config['MONGODB_DB']]
-    app.session_interface = MongoDBSessionInterface(app, mongo_sess, 'sessions')
-    # cache.clear()
+# with app.app_context():
+#     # cache.clear()
 
 if __name__ == "__main__":
     app.run(host="localhost", port="5000", debug=True)
