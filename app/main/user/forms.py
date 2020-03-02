bcfdb4b1a81c9cf8240f16addd783fd508d1c7b3
phuongvuong98 <vuongnp@athena.studio>
2020-03-02 14:34:18 +0700

    completed profile

--- app/main/user/forms.py
+++ app/main/user/forms.py
@@ -1,26 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField
-from wtforms.fields.html5 import EmailField
 from wtforms.validators import (DataRequired, Regexp, Length, Email)
 
 
-class AuthForm(FlaskForm):
-    email = EmailField(
-        'email',
-        [DataRequired(), Regexp(r'[^@]+@[^@]+\.[^@]+'), Length(min=4, max=25)]
-    )
-    password = PasswordField(
-        'password',
+class UpdatePswForm(FlaskForm):
+    old_password = PasswordField(
+        'old_password',
         validators=[DataRequired(), Length(min=3, max=25)]
     )
-
-
-class SignupForm(AuthForm):
-    password_confirm = PasswordField(
-        'password_confirm',
+    new_password_1 = PasswordField(
+        'new_password_1',
         validators=[DataRequired(), Length(min=3, max=25)]
     )
-
-
-class LoginForm(AuthForm):
-    pass
+    new_password_2 = PasswordField(
+        'new_password_2',
+        validators=[DataRequired(), Length(min=3, max=25)]
+    )
