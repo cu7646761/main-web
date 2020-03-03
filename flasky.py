ecac66806b1b6a8f0d83d61ddea1d03ba09b3117
hoanganpro2030 <1610132@hcmut.edu.vn>
2020-03-03 11:28:00 +0700

    fix port

--- flasky.py
+++ flasky.py
@@ -11,4 +11,4 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 if __name__ == "__main__":
     # app.run(debug=True)
     # app.run()
-    app.run(host="127.0.0.1", port="5001", debug=True)
+    app.run(host="127.0.0.1", port="5000", debug=True)
